9bb54b909e47f2eb0c3579fda491c72752041941
MohitKalshan <mohitkalshan14.mk@gmail.com>
2023-05-10 18:47:53 +0530

    Routes and routes added

--- src/component/App.js
+++ src/component/App.js
@@ -1,8 +1,14 @@
+import { Routes , Route } from "react-router-dom";
+import { Navbar, Home, CreatePost, PostDetail } from "./";
 
 function App() {
   return (
-    <div className="App">
-      hello blog
+    <div className="container">
+      <Routes>
+        <Route exact path="/" Component={<Home/>} />
+        <Route exact path="/post/:postid" Component={<PostDetail/>} />
+        <Route exact path="/create-post" Component={<CreatePost/>} />
+      </Routes>
     </div>
   );
 }
